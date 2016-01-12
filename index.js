b245ae830043641c7c507885a5d4c829847d067e
Samir Mehta <sammehta88@gmail.com>
2016-01-12 10:41:40 -0500

    add spec folder for jasmine testing

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+server.listen(3000, function(){
+  console.log('listening on port 3000');
+});
---
+++ spec/UserSpec.js
@@ -0,0 +1,66 @@
+describe("User", function() {
+  var io = require('socket.io-client');
+  var socket;
+
+  beforeEach(function(done){
+    socket = io.connect('http://localhost:3000', {
+      'reconnection delay' : 0,
+      'reopen delay' : 0,
+      'force new connection' : true
+    });
+
+    socket.on('connect', function(){
+      console.log('worked...');
+      done();
+    });
+
+    socket.on('disconnect', function() {
+      console.log('disonnceted... ');
+    });
+  });
+
+
+  // it("should have a corresponding cat on the page", function(){
+  //
+  // });
+  //
+  // it("should be able to see other users cats on the page", function(){
+  //
+  // });
+
+  describe("when a user goes to this site", function(){
+    it("a userId should be generated", function(done){
+
+    });
+
+    it("should receive a userID", function(done){
+
+    });
+
+    it("should be the only user with its userID", function(){
+
+    });
+  });
+
+  describe("when a user exits the site", function(){
+    it("should be deleted from memory", function(){
+
+    });
+  });
+
+});
+
+describe("Cat", function(){
+
+  it("should be associated with a userID", function(){
+
+  });
+
+  it("should respond to input from the user with the same userID", function(){
+
+  });
+
+  it("should not respond to input from other users", function(){
+
+  });
+});
