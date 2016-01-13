18306d65a42f6dd03c79e79c7f3c834ff0a36ac9
Samir Mehta <sammehta88@gmail.com>
2016-01-13 00:12:18 -0500

    FIX: change port for heroku deployment

--- index.js
+++ index.js
@@ -61,6 +61,6 @@ io.on('connection', function(socket){
   });
 });
 
-server.listen(3000, function(){
-  console.log('listening on port 3000');
+server.listen(process.envPORT || 3000, function(){
+  console.log('listening');
 });
