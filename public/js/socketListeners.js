750e8b4e26aa4ed8c19111e0c453f14c77e96792
Samir Mehta <sammehta88@gmail.com>
2016-01-15 12:38:52 -0500

    FIX: duplicate setSocketListeners

--- public/js/socketListeners.js
+++ public/js/socketListeners.js
@@ -10,9 +10,3 @@ function setSocketListeners(socket){
     helpers.setCatScore(data.score);
   });
 }
-
-function setSocketListeners(socket){
-  socket.on('gameUpdated:catScore', function(data){
-    helpers.setCatScore(data.score);
-  });
-}
